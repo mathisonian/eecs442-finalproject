22defd73c78c595f4abb25a8e0f1ac9da0f9b99c
@mpconlen <mc@mathisonian.com>
2011-12-10 00:19:08 -0500

    Graph cut completed

--- graphCut.m
+++ graphCut.m
@@ -1,8 +1,12 @@
-function [ cutMask ] = graphCut( adjMatrix )
+function [ cutMask ] = graphCut( adjMatrix, contextMap )
+    
+    % Remove the sink/source info
+    adjMatrix = adjMatrix(1:end-2,1:end-2);
     
     normalized = adjMatrix / sum(adjMatrix(:));
     w = size(adjMatrix,1);
-    h = size(adjMatrix,h);
+    h = size(adjMatrix,2);
+    contractPointers = cell(1,w);
     
     while max(normalized(:)) < 1
         normalized = normalized / sum(normalized(:));
@@ -18,6 +22,11 @@ function [ cutMask ] = graphCut( adjMatrix )
                     normalized(:,j) = zeros(w,1);
                     normalized(j,:) = zeros(1,h);
                     normalized(i,i) = 0;
+                    
+                    % Store in i that we added j to it
+                    contractPointers{i} = [contractPointers{i}, contractPointers{j}, j];
+                    contractPointers{j} = [];
+                    
                     contracted = 1;
                     break;
                 end
@@ -29,6 +38,19 @@ function [ cutMask ] = graphCut( adjMatrix )
     end
     
     % return a mask of shit that is in the new / old image
+    [r,c] = find(contextMap);
+    cutMask = zeros(size(contextMap));
+    cut=0;
+    for i = 1:w
+        cur = contractPointers{i};
+        if(size(cur,1) > 0)
+            cut = cut + 1;
+        end
+        for pointer = cur
+            cutMask(r(pointer), c(pointer)) = cut;
+        end
+    end
+    
     
 end
 
