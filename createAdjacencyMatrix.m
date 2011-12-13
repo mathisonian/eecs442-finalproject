846fe87d310a6c70c40481919e6a11c67b1e16d1
Matthew Conlen <mc@mathisonian.com>
2011-12-12 21:13:21 -0500

    Added file for seam filling in

--- createAdjacencyMatrix.m
+++ createAdjacencyMatrix.m
@@ -4,7 +4,9 @@ function [ adjMatrix ] = createAdjacencyMatrix(img, match, context, mask, x_t, y
     w = s(1);
     h = s(2);
     
-    
+    totalPoints=0;
+    tX=0;
+    tY=0;
     contextMap = zeros(w,h);
     count = 1;
     for i = 1:w
@@ -13,8 +15,16 @@ function [ adjMatrix ] = createAdjacencyMatrix(img, match, context, mask, x_t, y
                 contextMap(i,j) = count;
                 count = count + 1;
             end
+            if mask(i,j) == 0
+                totalPoints = totalPoints + 1;
+                tX = tX + i;
+                tY = tY + j;
+            end
         end
     end
+    
+    centerOfGravity = [tX/totalPoints, tY/totalPoints];
+    DISTANCE_FACTOR = .3;
     
     adjMatrix = zeros(count+1,count+1);
     originalImage = count;
@@ -31,7 +41,7 @@ function [ adjMatrix ] = createAdjacencyMatrix(img, match, context, mask, x_t, y
                 % test pixel to the left
                 if i > 1 && context(i-1,j) == 1
                     % create edge p->q
-                    adjMatrix(contextMap(i,j),contextMap(i-1,j)) = getGradientMagnitude(i,j,i-1,j, lab1, lab2, x_t, y_t);
+                    adjMatrix(contextMap(i,j),contextMap(i-1,j)) = DISATANCE_FACTOR*(sqrt((i-centerOfGravity(1))^2 + (j-centerOfGravity(2))^2)) + getGradientMagnitude(i,j,i-1,j, lab1, lab2, x_t, y_t);
                 elseif i > 1 && mask(i-1,j) == 0 
                     % create edge p->hole
                     adjMatrix(contextMap(i,j),hole) = Inf;
@@ -43,7 +53,7 @@ function [ adjMatrix ] = createAdjacencyMatrix(img, match, context, mask, x_t, y
                 % test pixel above
                 if j > 1 && context(i, j-1) == 1
                     % create edge p->q
-                    adjMatrix(contextMap(i,j),contextMap(i,j-1)) = getGradientMagnitude(i,j,i,j-1, lab1, lab2, x_t, y_t);
+                    adjMatrix(contextMap(i,j),contextMap(i,j-1)) = DISATANCE_FACTOR*(sqrt((i-centerOfGravity(1))^2 + (j-centerOfGravity(2))^2)) + getGradientMagnitude(i,j,i,j-1, lab1, lab2, x_t, y_t);
                 elseif j > 1 && mask(i,j-1) == 0 
                     % create edge p->hole
                     adjMatrix(contextMap(i,j),hole) = Inf;
@@ -54,7 +64,7 @@ function [ adjMatrix ] = createAdjacencyMatrix(img, match, context, mask, x_t, y
                 % test pixel to the right
                 if i < w && context(i+1, j) == 1
                     % create edge p->q
-                    adjMatrix(contextMap(i,j),contextMap(i+1,j)) = getGradientMagnitude(i,j,i+1,j, lab1, lab2, x_t, y_t);
+                    adjMatrix(contextMap(i,j),contextMap(i+1,j)) = DISATANCE_FACTOR*(sqrt((i-centerOfGravity(1))^2 + (j-centerOfGravity(2))^2)) + getGradientMagnitude(i,j,i+1,j, lab1, lab2, x_t, y_t);
                 elseif i < w && mask(i+1,j) == 0 
                     % create edge p->hole
                     adjMatrix(contextMap(i,j),hole) = Inf;
@@ -65,7 +75,7 @@ function [ adjMatrix ] = createAdjacencyMatrix(img, match, context, mask, x_t, y
                 % test pixel below
                 if j < h && context(i, j+1) == 1
                     % create edge p->q
-                    adjMatrix(contextMap(i,j),contextMap(i,j+1)) = getGradientMagnitude(i,j,i,j+1, lab1, lab2, x_t, y_t);
+                    adjMatrix(contextMap(i,j),contextMap(i,j+1)) = DISATANCE_FACTOR*(sqrt((i-centerOfGravity(1))^2 + (j-centerOfGravity(2))^2)) + getGradientMagnitude(i,j,i,j+1, lab1, lab2, x_t, y_t);
                 elseif j < h && mask(i,j+1) == 0 
                     % create edge p->hole
                     adjMatrix(contextMap(i,j),hole) = Inf;
---
+++ getMatchingImagesSeam.m
@@ -0,0 +1,67 @@
+function [ matches, imgMask ] = getMatchingImagesSeam( img, indir, extendVal )
+    
+    GIST_WEIGHT_CONSTANT = 2;
+    COLOR_WEIGHT_CONSTANT = 1;
+    NUM_MATCHES = 100;
+    gists = cell(1,1000000);
+    imgs = cell(1,1000000);
+    NUM_IMAGES = getNumImages(indir, 0)
+
+    seamMask = getSeam(img, 1);
+    origSize = size(img,2);
+    img = [img zeros(size(img,1), extendVal, 3)];
+    imgMask = ones(size(img,1), size(img,2));
+    for x=1:size(seamMask,1)
+        for y=1:size(seamMask,2)
+            if seamMask(x,y) == 1
+                img(x,end-(origSize-y):end,:) = img(x,y:origSize,:);
+                img(x,y:y+extendVal-1,:) = zeros(1,extendVal,3);
+                imgMask(x,y:y+extendVal-1,:) = zeros(1,extendVal);
+            end
+        end
+    end
+    
+    imshow(img);
+    
+    imgGist = getGistMask(img, imgMask);
+    tic
+    diffs = zeros(1,NUM_IMAGES);
+    parfor i=1:NUM_IMAGES
+        testGist = gists{i};
+        testImg = imread(imgs{i});
+        gistSSD = getSSD(imgGist, testGist);
+%         disp('getting color ssd');
+%         colorSSD = getColorDiff(img, testImg);
+        diffs(i) = GIST_WEIGHT_CONSTANT*gistSSD;
+%                         + COLOR_WEIGHT_CONSTANT*colorSSD;
+    end
+    
+    [B,IX] = sort(diffs,'descend');
+    matches = cell(1,NUM_MATCHES);
+    for i=1:NUM_MATCHES
+        matches{i} = imgs{IX(i)};
+    end
+    
+    toc
+    
+    function count = getNumImages(indir, start)
+        d = dir(indir);
+        count = start;
+        
+        for j=3:size(d)
+            n = [indir '/' d(j).name];
+            if(d(j).isdir)
+                count = count + getNumImages(n, count);
+            else
+                [a,b,c]=fileparts(n);
+                if(strcmp(c,'.gist'))
+                    count = count+1;
+                    g = load(n, '-mat');
+                    gists{count} = g.gist;
+                    imgs{count} = n(1:end-5);
+                end
+            end
+        end
+    end
+end
+
