c6adf745bf3918a6755be4633a958fb70498433e
Michael Gisi <gisi@umich.edu>
2011-12-13 00:17:38 -0500

    Worked around possible error with contexts on image edges

--- solvePoisson2.m
+++ solvePoisson2.m
@@ -39,38 +39,43 @@ function [ out ] = solvePoisson2(src,tgt,context,select,xt,yt)
     
     %iterate through A
     cnt=0;
-    for i=2:width-1
-        for j=2:height-1
+    for i=1:width
+        for j=1:height
             if(select(i,j)==0 || context(i,j) == 2)
                 cnt = cnt + 1;
                 A(cnt,cnt) = 4;
                 %check for neighbors
+                if(j<height)
                 if(select(i,j+1) == 0 || context(i,j+1) == 2)
                     p = c2v(i,j+1);
                     A(cnt, p) = -1;
                 else
                     b(cnt) = b(cnt) + tgt(i,j+1);
                 end
-                
+                end
+                if(j>1)
                 if(select(i,j-1) == 0 || context(i,j-1) == 2)
                     p = c2v(i,j-1);
                     A(cnt, p) = -1;
                 else
                     b(cnt) = b(cnt) + tgt(i,j-1);
                 end
-                
+                end
+                if(i<width)
                 if(select(i+1,j) == 0 || context(i+1,j) == 2)
                     p = c2v(i+1,j);
                     A(cnt, p) = -1;
                 else
                     b(cnt) = b(cnt) + tgt(i+1,j);
                 end
-                
+                end
+                if(i>1)
                 if(select(i-1,j) == 0 || context(i-1,j) == 2)
                     p = c2v(i-1,j);
                     A(cnt, p) = -1;
                 else
                     b(cnt) = b(cnt) + tgt(i-1,j);
+                end
                 end
                 %add guidance vector field
                 b(cnt) = b(cnt) + src(i-xt,j-yt);
@@ -82,7 +87,10 @@ function [ out ] = solvePoisson2(src,tgt,context,select,xt,yt)
     %solve for x
     
     x = A\(b');
+    %x = pinv(A)*(b');
     %x = lscov(A,b);
+    
+    %x = bicg(A, b', [], 400);
     
     out = tgt;
     %build output image
